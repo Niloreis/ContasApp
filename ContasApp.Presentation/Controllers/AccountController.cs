a9f7d31e7440b2ae1654d1b96936e46ca67d4572
Nilo Reis <nilogreis@gmail.com>
2023-07-03 10:58:29 -0300

    feat:Criação das paginas  Login,Register ,ForgotPassoword

---
+++ ContasApp.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContasApp.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        /// <summary>
+        /// Metedo para abrir pagina /Account/Login
+        /// </summary>
+        /// 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Metedo para abrir pagina /Account/Register
+        /// </summary>
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Metedo para abrir pagina /Account/ForgotPassword
+        /// </summary>
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }  
+}
